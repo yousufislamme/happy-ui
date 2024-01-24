98a4556c3e6bf5473e82d987d158222f9fd49bc4
yousufislamme <online.okk@gmail.com>
2024-01-24 22:24:07 +0600

    some change

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 import * as React from "react"
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <Hero />
     </main>
   );
 }
